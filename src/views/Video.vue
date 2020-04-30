1ab74ff499f06c2a80085ad7e0058c920d9d53d0
Yoshiaki Satoh <y-sato@mediba.jp>
2020-04-30 16:23:24 +0900

    Mac死ね(直球

--- src/views/Video.vue
+++ src/views/Video.vue
@@ -2,9 +2,9 @@
   <v-content>
     <MultiViewMoviePlayer
       videopath1="ミクリンレンでハレ晴レユカイ1_x264_low.mp4"
-      videopath2="ミクリンレンでハレ晴レユカイ2_x264_low.mp4"
-      videopath3="ミクリンレンでハレ晴レユカイ3_x264_low.mp4"
-      videopath4="ミクリンレンでハレ晴レユカイ4_x264_low.mp4"
+      videopath2="ミクリンレンでハレ晴レユカイ2_x264_low.mp4"
+      videopath3="ミクリンレンでハレ晴レユカイ3_x264_low.mp4"
+      videopath4="ミクリンレンでハレ晴レユカイ4_x264_low.mp4"
     />
     <!-- MultiViewMoviePlayer
       videopath2="http://3qjcxj7d6rmwu6.data.mediastore.ap-northeast-1.amazonaws.com/livea/main.m3u8"
